7a184d76db6e3a46a802dffd80c1c7c007b931c4
vinutha <shreevinutha.m@gmail.com>
2026-09-19 00:05:14 +0530

    Build initial Splitzy shell

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main>
+      <p>Splitzy</p>
+      <h1>Splitzy</h1>
+
+      <button>Start a split →</button>
+    </main>
+  )
+}
+
+export default App
